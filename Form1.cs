394d7914427464554a2051170b0ee941204f58aa
Usamaansari88 <97385480+Usamaansari88@users.noreply.github.com>
2022-04-03 17:07:36 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace inner_join
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        string con = "Data Source=DESKTOP-UR1SGFT\\SQLEXPRESS;Initial Catalog=Students;Integrated Security=True";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection c = new SqlConnection(con);
+            c.Open();
+
+            string query = " Select stdinfo.RollNo,stdinfo.Name,stdinfo.Class,stdinfo.Section,stdmarks.Subject1,stdmarks.Subject2,stdmarks.Subject3,stdmarks.Subject4 from stdinfo inner join stdmarks on stdinfo.RollNo=stdmarks.RollNo where stdinfo.RollNo='"+textBox1.Text.ToString()+"'";
+
+            SqlCommand cmd = new SqlCommand(query, c);
+            cmd.ExecuteNonQuery();
+
+            DataTable data = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(data);
+            dataGridView1.DataSource = data;
+            c.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
